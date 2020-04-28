0b616fc96cf8e95e8d640a72e878aa9ca7d533f9
TheDankMatter <piotriwo@gmail.com>
2020-04-28 21:18:52 +0200

    Code refactor with -squash

---
+++ Paint_Programing/Paint_Programing.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+/****************************** Module Header ******************************\
+* Module Name:	Paint_Programing.h
+* Project:		Paint
+* Verson: 0.1
+\***************************************************************************/
+#include "Resource.h"
+#include <string>
+#include "DrawObj.h"
+// inlcude Funtions Using Paint_Programing
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK    ChildWndProc(HWND, UINT, WPARAM, LPARAM);
+void				AutoScroll(HWND, int, int, int, int, RECT);
+RECT				GetLocalCoordinates(HWND hWnd);
+void				CleanObjects(HWND);
+void				SetTitle(std::string, HWND);
+void				ChangeToolsSelectionState(int, HMENU);
+void				ChangeColorsSelectionState(int, HMENU);
+void				ChangeBGSelectionState(int, HMENU);
+void				ChangeLineSelectionState(int, HMENU);
+void				AutoScrollObject(HWND, const DrawObj*, int, int, RECT);
+void				AutoScrollObjectResize(HWND, const DrawObj*, int, int, RECT);
+void				AutoScrollObjectWhenDrawing(HWND, const DrawObj*, int, int, RECT);
